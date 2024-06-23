3ac6a27b81f273ae9974408dfd082d3f899f6626
Andre Becker <becker-andre@outlook.com>
2024-06-24 01:30:16 +0200

    String name changes for import files

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,20 +1,20 @@
 import yaml from "js-yaml";
 
 export async function fetchGephi() {
-    const gephiResponse = await fetch("/gephi.json");
+    const gephiResponse = await fetch("gephi.json");
     const gephi = await gephiResponse.json();
     return gephi;
 }
 
 export async function fetchGephiIO() {
-    const gephiIOResponse = await fetch("/gephiIO.yaml");
+    const gephiIOResponse = await fetch("gephiIO.yaml");
     const gephiIOString = await gephiIOResponse.text();
     const gephiIO = yaml.load(gephiIOString);
     return gephiIO;
 }
 
 export async function fetchGeneralInfo() {
-    const generalInfoResponse = await fetch("/generalInfo.json");
+    const generalInfoResponse = await fetch("generalInfo.json");
     const generalInfo = await generalInfoResponse.json();
     return generalInfo;
 }
